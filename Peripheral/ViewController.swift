d214fd3a713b4180d9f7b4e398ef15d6b33cc1f8
Uy Nguyen Long <nluy@FOSSIL.COM>
2018-01-05 11:04:17 +0700

    Initial Commit

---
+++ Peripheral/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Peripheral
+//
+//  Created by Uy Nguyen Long on 1/5/18.
+//  Copyright © 2018 Uy Nguyen Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
